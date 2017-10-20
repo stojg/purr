da5d1e2420418489ce14c10e741276d79a099046
Stig Lindqvist <stojg.lindqvist@gmail.com>
2017-10-21 12:11:19 +1300

    improve test coverage of Config

--- config_test.go
+++ config_test.go
@@ -9,17 +9,24 @@ func TestNewConfig(t *testing.T) {
 		return
 	}
 
-	if config.GitHubToken != "super_secret" {
-		t.Errorf("expected GitHubToken to be 'super_secret', got '%s'", config.GitHubToken)
+	validationErrors := config.validate()
+	if len(validationErrors) != 0 {
+		for _, err := range validationErrors {
+			t.Errorf("Did not expect validation error: %+v", err)
+		}
+		return
+	}
+
+	if config.GitHubToken != "secret_github_token" {
+		t.Errorf("expected GitHubToken to be 'secret_github_token', got '%s'", config.GitHubToken)
 	}
 
 	if len(config.GitHubOrganisations) != 1 {
 		t.Errorf("Expected 1 GitHubOrganisations, got %d", len(config.GitHubRepos))
 		return
 	}
-
-	if config.GitHubOrganisations[0] != "twitter" {
-		t.Errorf("Expected GitHubOrganisationsto be '%s', got '%s'", "twitter", config.GitHubOrganisations[0])
+	if config.GitHubOrganisations[0] != "facebook" {
+		t.Errorf("Expected GitHubOrganisationsto be '%s', got '%s'", "facebook", config.GitHubOrganisations[0])
 		return
 	}
 
@@ -27,7 +34,6 @@ func TestNewConfig(t *testing.T) {
 		t.Errorf("Expected 1 GitHubUsers, got %d", len(config.GitHubUsers))
 		return
 	}
-
 	if config.GitHubUsers[0] != "stojg" {
 		t.Errorf("Expected first GitHubUsers to be '%s', got '%s'", "stojg", config.GitHubUsers[0])
 		return
@@ -37,13 +43,44 @@ func TestNewConfig(t *testing.T) {
 		t.Errorf("Expected 2 GitHubRepos, got %d", len(config.GitHubRepos))
 		return
 	}
+	if config.GitHubRepos[0] != "user1/repo1" {
+		t.Errorf("Expected first GitHubRepos to be '%s', got '%s'", "user1/repo1", config.GitHubRepos[0])
+		return
+	}
+	if config.GitHubRepos[1] != "user2/repo1" {
+		t.Errorf("Expected second GitHubRepos to be '%s', got '%s'", "user2/repo1", config.GitHubRepos[1])
+		return
+	}
 
-	if config.GitHubRepos[0] != "facebook/emitter" {
-		t.Errorf("Expected first GitHubRepos to be '%s', got '%s'", "facebook/emitter", config.GitHubRepos[0])
+	if config.GitlabURL != "https://www.example.com" {
+		t.Errorf("expected GitlabURL to be 'https://www.example.com', got '%s'", config.GitlabURL)
+	}
+
+	if config.GitLabToken != "secret_gitlab_token" {
+		t.Errorf("expected GitLabToken to be 'secret_gitlab_token', got '%s'", config.GitLabToken)
+	}
+
+	if len(config.GitLabRepos) != 2 {
+		t.Errorf("Expected 2 GitLabRepos, got %d", len(config.GitLabRepos))
 		return
 	}
-	if config.GitHubRepos[1] != "facebook/buck" {
-		t.Errorf("Expected second GitHubRepos to be '%s', got '%s'", "facebook/buck", config.GitHubRepos[1])
+	if config.GitLabRepos[0] != "project1/repo1" {
+		t.Errorf("Expected first GitLabRepos to be '%s', got '%s'", "project1/repo1", config.GitLabRepos[0])
 		return
 	}
+	if config.GitLabRepos[1] != "project2/repo1" {
+		t.Errorf("Expected second GitLabRepos to be '%s', got '%s'", "project2/repo1", config.GitLabRepos[1])
+		return
+	}
+
+	if config.SlackChannel != "myteamchat" {
+		t.Errorf("Expected second SlackChannel to be 'myteamchat', got '%s'", config.SlackChannel)
+		return
+	}
+
+	if config.SlackToken != "secret_slack_token" {
+		t.Errorf("Expected second SlackToken to be 'secret_slack_token', got '%s'", config.SlackToken)
+		return
+	}
+
 }
